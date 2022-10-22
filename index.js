d9eda544fc65e17f047303cd57c32b93bcd87df8
ajac13 <107587200+ajac13@users.noreply.github.com>
2022-10-22 14:45:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-
+// Requiring module
+const express = require('express');
+ 
+// Creating express object
+const app = express();
+ 
+// Handling GET request
+app.get('/', (req, res) => {
+    res.send('Group 47 Restaurant Management System for COSC 4351 class.')
+    res.end()
+})
+ 
+// Port Number
+const PORT = process.env.PORT ||3000;
+ 
+// Server Setup
+app.listen(PORT,console.log(
+  `Server started on port ${PORT}`));
